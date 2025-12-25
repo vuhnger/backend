14fc094c5b33c84394bc84ba9aa561564f976caf
vuhger <victor.uhnger@gmail.com>
2025-12-25 14:27:14 +0100

    update cors config

--- apps/strava/main.py
+++ apps/strava/main.py
@@ -35,7 +35,8 @@ origins = [
     "https://vuhnger.dev",
     "https://vuhnger.github.io",
     "http://localhost:5173",
-    "http://localhost:3000",
+    "http://localhost:3000"
+    "https://localhost:3000",
 ]
 
 app.add_middleware(
